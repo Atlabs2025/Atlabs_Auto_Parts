cb720eb3414a33ac08e159b1cdca7d38268c1b9f
atlabs <merlinthomasofficial@gmail.com>
2026-02-02 11:56:19 +0400

    modules uploaded

---
+++ atlbs_api/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+###################################################################################
+
+# Author       : Merlin Thomas & Nadira Anas
+# Copyright(c) :  2023-Present Zinfog Codelabs Pvt Ltd (<https://www.zinfog.com>).
+# License      :  LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+# This program is free software:
+# you can modify it under the terms of the GNU Lesser General Public License (LGPL) as
+# published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+###################################################################################
+
+{
+    "name": "KAVAK Rest API",
+    "summary": """ Restful API for KAVAK """,
+    "category": "atlabs",
+    "version": "18.0.1.0.0",
+    "author": "Atlabs Cloud Technology",
+    "license": "LGPL-3",
+    "website": "https://www.atlabs.com",
+    "description": """ Restful API for KAVAK """,
+    "depends": ['base', 'google_sheet_vehicle_sync_full','atlbs_parts_management'],
+    "data": [],
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+}
---
+++ atlbs_api/controllers/db_config.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo import registry as registry_get
+
+class GetDBName():
+
+    def getdbname(self):
+        return 'Odoo_Kavak_jan_30'
